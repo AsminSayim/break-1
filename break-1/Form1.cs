84883f7baa462525ade7325bf61f1a0e85612ec5
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2023-11-20 11:34:29 +0300

    Add files via upload

---
+++ break-1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace break_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aranan=txtAra.Text;
+            int sayac = 0;
+
+            for (int i=0; i<listBox1.Items.Count; i++)
+            {
+                if(aranan == listBox1.Items[i].ToString())
+                {
+                    sayac++;
+                }
+            }
+            if(sayac == 0)
+            {
+                MessageBox.Show("Öğrenci bulunamadı");
+            }
+            else
+            {
+                MessageBox.Show(sayac + "Adet öğrenci bulundu");
+            }
+        }
+    }
+}
